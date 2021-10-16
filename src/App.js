39b9a19ea37f00ef186ad0f58a51bc032cfa8ce6
Viktoriya Shpachynska <viktoriya.shpachynska@gmail.com>
2021-10-16 15:38:19 +0200

    adds App refactoring

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
+// import useLocalStorage from './hooks/useLocalStorage';
 import shortid from "shortid";
 import ContactList from "./components/ContactList/ContactList";
 import Filter from "./components/Filter/Filter";
@@ -10,112 +11,85 @@ import IconButton from "./components/IconButton/IconButton";
 import { ReactComponent as AddIcon } from "./components/IconButton/add.svg";
 import Container from "./components/Container/Container";
 
-class App extends Component {
-  state = {
-    contacts: [
-      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-    ],
-    filter: "",
-    showModal: false,
-  };
+const initialContacts = [
+  { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+  { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+  { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+  { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+];
 
-  //====================== retrieve from local storage ========================
-  componentDidMount() {
-    const contacts = localStorage.getItem("contacts");
-    const parsedContacts = JSON.parse(contacts);
+export default function App() {
+  const [contacts, setContacts] = useState(
+    () => JSON.parse(window.localStorage.getItem("contacts")) ?? initialContacts
+  );
+  const [filter, setFilter] = useState("");
+  const [showModal, setShowModal] = useState(false);
 
-    if (parsedContacts) {
-      this.setState({ contacts: parsedContacts });
-    }
-  }
-  //===================================================================
-
-  //====================== add to local storage ========================
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
-    }
-  }
-  //===================================================================
-
-  addContact = (data) => {
+  const addContact = (data) => {
     const contact = {
       id: shortid.generate(),
       name: data.name,
       number: data.number,
     };
-    if (this.state.contacts.find((contact) => contact.name === data.name)) {
+    if (contacts.find((contact) => contact.name === data.name)) {
       alert(`${data.name} is already in contacts`);
       return;
     } else {
-      this.setState((prevState) => ({
-        contacts: [contact, ...prevState.contacts],
-      }));
+      setContacts([contact, ...contacts]);
     }
-    this.toggleModal();
+    toggleModal();
   };
 
-  changeFilter = (e) => {
-    this.setState({ filter: e.currentTarget.value });
+  const changeFilter = (e) => {
+    setFilter(e.currentTarget.value);
   };
 
-  getVisibleContacts = () => {
-    const { filter, contacts } = this.state;
+  const getVisibleContacts = () => {
     const normalizedFilter = filter.toLowerCase();
     return contacts.filter((contact) =>
       contact.name.toLowerCase().includes(normalizedFilter)
     );
   };
 
-  deleteContact = (contactId) => {
-    this.setState((prevState) => ({
-      contacts: prevState.contacts.filter(
-        (contact) => contact.id !== contactId
-      ),
-    }));
+  const deleteContact = (contactId) => {
+    setContacts(() => contacts.filter((contact) => contact.id !== contactId));
   };
+
+  useEffect(() => {
+    localStorage.setItem("contacts", JSON.stringify(contacts));
+  }, [contacts]);
 
   //============ modal window ============================
-  toggleModal = () => {
-    this.setState((state) => ({
-      showModal: !state.showModal,
-    }));
+  const toggleModal = () => {
+    setShowModal(!showModal);
   };
   //======================================================
+  const visibleContacts = getVisibleContacts();
+  return (
+    <Container>
+      <h1 className="title">Phonebook</h1>
+      <IconButton onClick={toggleModal}>
+        <AddIcon width="40" height="40" fill="#fff" />
+      </IconButton>
 
-  render() {
-    const { filter, showModal } = this.state;
-    const visibleContacts = this.getVisibleContacts();
-    return (
-      <Container>
-        <h1 className="title">Phonebook</h1>
-        <IconButton onClick={this.toggleModal}>
-          <AddIcon width="40" height="40" fill="#fff" />
-        </IconButton>
-
-        {showModal && (
-          <Modal onClose={this.toggleModal}>
-            <div className="form-section">
-              <div className="form">
-                <Form onSubmit={this.addContact} />
-              </div>
+      {showModal && (
+        <Modal onClose={toggleModal}>
+          <div className="form-section">
+            <div className="form">
+              <Form onSubmit={addContact} />
             </div>
-          </Modal>
-        )}
+          </div>
+        </Modal>
+      )}
 
-        <div className="contacts-section">
-          <b className="title">Contacts</b>
-          <Filter value={filter} onChange={this.changeFilter} />
-          <ContactList
-            contacts={visibleContacts}
-            onDeleteContact={this.deleteContact}
-          />
-        </div>
-      </Container>
-    );
-  }
+      <div className="contacts-section">
+        <b className="title">Contacts</b>
+        <Filter value={filter} onChange={changeFilter} />
+        <ContactList
+          contacts={visibleContacts}
+          onDeleteContact={deleteContact}
+        />
+      </div>
+    </Container>
+  );
 }
-export default App;
